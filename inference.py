a101f61215857bc285e755c7b2040c80fdb7d00c
Chaskar Prasad Bhausaheb <72083319+Prasadchaskar@users.noreply.github.com>
2021-12-28 10:49:06 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -5,5 +5,6 @@ import math
 model = pickle.load(open('crabage.pkl', 'rb'))
 def predict(df):
     df = df[['Length', 'Diameter', 'Height', 'Weight', 'Shucked Weight','Viscera Weight','Shell Weight']]
+    numpy_array = df.to_numpy()
     predictions = model.predict(numpy_array)
     return list(np.floor(predictions))
